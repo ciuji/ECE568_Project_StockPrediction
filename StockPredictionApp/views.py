e66829a646b05cdc35b441dee688bbe16026db26
ciuji's mbp <zuochaoji@gmail.com>
2019-05-02 20:35:08 -0400

    [modify] use real data

--- StockPredictionApp/views.py
+++ StockPredictionApp/views.py
@@ -20,13 +20,14 @@ app.add_url_rule(api_version+'/stockdata/<interval>/<symbol>', view_func=GetStoc
 def homepage():
     if(request.method=='POST'):
         search_symbol=request.form['search']
-        #search_data=GetStock.search(search_symbol)
+        search_data=GetStock.search(search_symbol)
         print("user searching symbol: "+search_symbol)
         if(search_symbol==''):
             print('no query')
             return render_template('stock_chart.html')
-        if(search_symbol!='aa'):
-            return render_template('stock_chart.html',data=json.dumps(get_plot_data()),stock_name=search_symbol)
+        print(search_data)
+        if search_data:
+            return render_template('stock_chart.html',data=json.dumps(search_data),stock_name=search_symbol)
         else:
             return render_template('stock_chart.html',sign='no such stock: '+search_symbol)
     else:
@@ -37,24 +38,4 @@ def homepage():
 def testfunc():
     return 'mystring'
 
-
-@app.route('/mystring')
-def mystring():
-    return 'my string'
-
-@app.route('/dataFromAjax')
-def dataFromAjax():
-    test = request.args.get('mydata')
-    print(test)
-    return 'dataFromAjax'
-
-@app.route('/mydict', methods=['GET', 'POST'])
-def mydict():
-    d = {'name': 'xmr', 'age': 18}
-    return jsonify(d)
-
-@app.route('/mylist')
-def mylist():
-    l = ['xmr', 18]
-    return jsonify(l)
 
